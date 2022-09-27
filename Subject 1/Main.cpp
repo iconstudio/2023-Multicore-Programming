d07fc90a507148a89abc686a4c190acb2fcad142
Iconer <yoyofa2@hotmail.com>
2022-09-27 19:05:57 +0900

    과제 1 수정

--- Subject 1/Main.cpp
+++ Subject 1/Main.cpp
@@ -1,6 +1,9 @@
 #include "Main.hpp"
 
-template<size_t Threads_count>
+constexpr int summary_target = 100000000;
+int summary = 0;
+
+template<int Threads_count>
 class BakeryLocker
 {
 public:
@@ -8,14 +11,31 @@ public:
 		: myFlags(), myLabals()
 	{}
 
-	void lock(const int id)
+	inline constexpr void lock(const int id)
 	{
 		myFlags[id] = true;
 
-		myLabals[id] = std::max(myLabals, myLabals + Threads_count);
+		myLabals[id] = std::max(myLabals, myLabals + Threads_count) + 1;
+
+		bool quit = true;
+		while (true)
+		{
+			for (int k = 0; k < Threads_count; k++)
+			{
+				if (k != id)
+				{
+					if (myFlags[k] && myLabals[k] < myLabals[id] && k < id)
+					{
+						quit = false;
+					}
+				}
+			}
+
+			if (quit) break;
+		}
 	}
 
-	void unlock(const int id)
+	inline constexpr void unlock(const int id)
 	{
 		myFlags[id] = false;
 	}
@@ -24,9 +44,26 @@ public:
 	std::array<int, Threads_count> myLabals;
 };
 
+template<int Thread_count>
+constexpr void Worker(const int id)
+{
+	BakeryLocker<Thread_counts> locker{};
+
+	const int local_target = summary_target / Thread_count;
+	const int local_times = local_target / 2;
+
+	for (int i = 0; i < local_times; i++)
+	{
+		CasLock(id);
+		summary += 2;
+		CasUnlock(id);
+	}
+}
+
 int main()
 {
-
+	Worker<2>(0);
+	Worker<2>(1);
 
 	return 0;
 }
